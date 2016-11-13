831272e55bb82f951c6746b79899755109112555
Marc-Alexandre Monette Molina <Marc-Alexandre Monette Molina>
2016-11-13 08:08:14 -0500

    focntionne

--- src/Cadriciel/DeroulementPartie.java
+++
@@ -1,9 +0,0 @@
-package Cadriciel;
-
-public abstract class DeroulementPartie {
-	public void jouerUnePartie(){
-	}
-	protected abstract void initJeu();
-	protected abstract void jouerUnTour();
-	protected abstract void terminerJeu();
-}
